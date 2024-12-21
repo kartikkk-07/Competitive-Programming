4ee3336a375c546a0ea642beab6a5e6ff7318dee
Kartik Magdum <119559799+kartikkk-07@users.noreply.github.com>
2024-12-21 13:33:46 +0530

    Create PyramidPattern.java

---
+++ PyramidPattern.java
@@ -0,0 +1,37 @@
+// Experiment Name: Print pyramid of alphabets from A to Z.
+// Name: Kartik Vijaykumar Magdum
+// Roll no.: 16
+// Batch: T1
+
+// Source Code
+
+import java.util.Scanner;
+public class PyramidPattern {
+
+    public static void printPattern(int num){
+        for(int i = 1; i <= num; i++){
+            for(int k = num; k >= i; k--){
+                System.out.print(" ");
+            }
+            char ch = 'A';
+            for(int j = 1; j <= i; j++){
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] github) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter number between 1 to 26 : ");
+        int num = sc.nextInt();
+
+        while (num < 1 || num > 26) {
+            System.out.println("Invalid input.....");
+            System.out.print("\nRe-enter number between 1 to 26 : ");
+            num = sc.nextInt();
+        }
+        printPattern(num);
+    }
+}
